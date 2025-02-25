323177c20ea9bebbbc2a69131e2221d09ad95022
shpark-99 <kk35834188@gmail.com>
2025-02-25 11:39:19 +0900

    Update script-admin.js

--- assets/script-admin.js
+++ assets/script-admin.js
@@ -1,22 +1,23 @@
-function initMap() {
-    if (!window.kakao || !window.kakao.maps) {
-        console.error("Kakao Maps SDK 로드 실패");
-        return;
-    }
-
+window.onload = function () {
     var container = document.getElementById('map');
     var options = {
         center: new kakao.maps.LatLng(37.5665, 126.9780), // 서울 기준
         level: 3
     };
+
     var map = new kakao.maps.Map(container, options);
-}
 
-function loadKakaoMap() {
-    var script = document.createElement("script");
-    script.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey=a50e768349b68eac92037c8858d7a462&libraries=services";
-    script.onload = initMap;
-    document.head.appendChild(script);
-}
-
-window.onload = loadKakaoMap;
+    // 현재 위치 가져오기
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var lat = position.coords.latitude;
+            var lon = position.coords.longitude;
+            var locPosition = new kakao.maps.LatLng(lat, lon);
+            
+            // 마커 표시
+            var marker = new kakao.maps.Marker({ position: locPosition });
+            marker.setMap(map);
+            map.setCenter(locPosition);
+        });
+    }
+};
