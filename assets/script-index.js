4176f7a4127c241209e7952a36daaec73260f1ec
shpark-99 <kk35834188@gmail.com>
2025-02-25 11:25:48 +0900

    Create script-index.js

---
+++ assets/script-index.js
@@ -0,0 +1,7 @@
+document.getElementById("adminBtn").addEventListener("click", function() {
+    window.location.href = "admin.html";
+});
+
+document.getElementById("userBtn").addEventListener("click", function() {
+    window.location.href = "user.html";
+});
